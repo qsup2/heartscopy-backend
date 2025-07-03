d0a92f5ce55658e3d93e258abfeb837ab762627e
heartscopy <qsup1128@gmail.com>
2025-07-03 20:56:39 +0900

    firebase config 수정

--- src/main/java/com/heartscopy/heartsocpyBeckEnd/config/FirebaseConfig.java
+++ src/main/java/com/heartscopy/heartsocpyBeckEnd/config/FirebaseConfig.java
@@ -7,8 +7,10 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class FirebaseConfig {
@@ -16,9 +18,12 @@ public class FirebaseConfig {
     @PostConstruct
     public void init() {
         try {
-            InputStream serviceAccount =
-                    new ClassPathResource("firebase/heartscopy-c5dae-firebase-adminsdk-k5m4j-db35d89050.json")
-                            .getInputStream();
+            String firebaseJson = System.getenv("FIREBASE_CONFIG_JSON");
+            if (firebaseJson == null) {
+                throw new IllegalStateException("FIREBASE_CONFIG_JSON 환경변수가 설정되지 않았습니다.");
+            }
+
+            InputStream serviceAccount = new ByteArrayInputStream(firebaseJson.getBytes(StandardCharsets.UTF_8));
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
